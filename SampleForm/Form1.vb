901e73e1cac419cf32c0a7bac91e41f92ee78d19
Shunjiro Yatsuzuka <shunjiro.yatsuzuka@uipath.com>
2019-06-22 17:37:51 +0900

    Init checkin

---
+++ SampleForm/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button.Click
+        LabelButtonValue.Text = CInt(LabelButtonValue.Text) + 1
+
+    End Sub
+
+    Private Sub CheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox.CheckedChanged
+        LabelCheckBoxValue.Text = CStr(CheckBox.Checked)
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox.SelectedIndexChanged
+        LabelCheckedListBoxValue.Text = CheckedListBox.SelectedIndex
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        LabelComboBoxValue.Text = ComboBox1.Text
+    End Sub
+
+    Private Sub DateTimePicker_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker.ValueChanged
+        LabelDateTimePickerValue.Text = DateTimePicker.Value
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        LabelListBoxValue.Text = ListBox1.SelectedItem.ToString
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LabelTreeView.Click
+
+    End Sub
+End Class
